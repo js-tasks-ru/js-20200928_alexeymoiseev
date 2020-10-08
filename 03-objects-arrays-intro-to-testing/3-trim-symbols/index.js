81bc19780e417bd293c55eb816f1820010deacd0
Alexey Moiseev <alexeymoiseevwork@gmail.com>
2020-10-08 17:54:43 +0300

    Реализация функции удаления лишних символов

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,27 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) return "";
+  if (size === undefined) return string;
 
+  let resString = "";
+  let sumSymbol = "";
+  let prevSymbol = "";
+
+
+  for (let curSymbol of string) {
+    if (curSymbol === prevSymbol){
+      if (sumSymbol.length < size) {
+        sumSymbol = sumSymbol + curSymbol;
+      }
+    } else {
+      resString = resString + sumSymbol;
+      sumSymbol = curSymbol;
+    }
+    prevSymbol = curSymbol;
+  }
+
+  resString = resString + sumSymbol;
+
+  return resString;
 }
